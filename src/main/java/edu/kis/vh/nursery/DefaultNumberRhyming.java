910ad442d589cc382a027dd95f0886aab8cce3d9
MiloszGrabowski1314 <251145@edu.p.lodz.pl>
2025-03-16 17:18:18 +0100

    Poprawa nazw klas

--- src/main/java/edu/kis/vh/nursery/DefaultNumberRhyming.java
+++ src/main/java/edu/kis/vh/nursery/DefaultNumberRhyming.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultNumberRhyming {
 
     private int[] NUMBERS = new int[12];
 
--- src/main/java/edu/kis/vh/nursery/FirstInFirstOutRhyming.java
+++ src/main/java/edu/kis/vh/nursery/FirstInFirstOutRhyming.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FirstInFirstOutRhyming extends DefaultNumberRhyming {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public DefaultNumberRhyming temp = new DefaultNumberRhyming();
     
     @Override
     public int countOut() {
