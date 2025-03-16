86d6067ed7a6eeb34abfb68e3d57786d5b9b1c31
MiloszGrabowski1314 <251145@edu.p.lodz.pl>
2025-03-16 17:35:32 +0100

    Poprawa nazw klas 2

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -1,15 +1,15 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
+import edu.kis.vh.nursery.DefaultNumberRhyming;
 
     public interface Rhymersfactory {
     
-        public defaultCountingOutRhymer GetStandardRhymer();
+        public DefaultNumberRhyming GetStandardRhymer();
         
-        public defaultCountingOutRhymer GetFalseRhymer();
+        public DefaultNumberRhyming GetFalseRhymer();
         
-        public defaultCountingOutRhymer GetFIFORhymer();
+        public DefaultNumberRhyming GetFIFORhymer();
         
-        public defaultCountingOutRhymer GetHanoiRhymer();
+        public DefaultNumberRhyming GetHanoiRhymer();
         
     }
